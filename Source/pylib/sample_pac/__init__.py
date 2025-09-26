8695d459372e37361341534294c54baa6a42f6b3
KSY0125 <tkddbs0125@naver.com>
2025-09-26 18:11:14 +0900

    리뉴얼

--- Source/pylib/sample_pac/__init__.py
+++ Source/pylib/sample_pac/__init__.py
@@ -1,6 +1,6 @@
 '''
 sample_pac/__init__.py
-sample_pac 패키지를 import 할 때 자동실행되는 파일입니다.
+sample_pac 패키지를 import 할 때 자동실행되는 파일입니다
 '''
 print("sample_pac 패키지를 로드했어요")
 
--- Source/pylib/sample_pac/ab/a.py
+++ Source/pylib/sample_pac/ab/a.py
@@ -1,4 +1,4 @@
-# python sample_pac/ab/a.py
+# sample_pac/ab/a.py
 
 def hello():
     print('sample_pac.ab.a모듈의 hello')
